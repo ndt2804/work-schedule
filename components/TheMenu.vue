c886f684b2ceef3ee9b1f4fcb19342099e91e5f8
ndt2804 <nduytan.uit.a3@gmail.com>
2023-11-09 21:53:07 +0700

    feat: implements layouts & index page

---
+++ components/TheMenu.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import TheSubMenu from './TheSubMenu.vue';
+
+export default {
+    components: {
+        TheSubMenu
+    },
+    props: {
+        model: {
+            type: Array,
+            default: () => ([])
+        }
+    },
+    emits: ['menuitem-click'],
+    methods: {
+        onMenuItemClick(event: any) {
+            this.$emit('menuitem-click', event);
+        },
+    }
+};
+</script>
+
+<template>
+    <div class="layout-menu-container">
+        <AppSubmenu :items="model" class="layout-menu" :root="true" @menuitem-click="onMenuItemClick" />
+    </div>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,131 @@
 <template>
-    <h1>HAHA</h1>
+    <div class="relative" id="home">
+        <div aria-hidden="true" class="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
+            <div class="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
+            <div class="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
+        </div>
+        <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+            <div class="relative pt-36 ml-auto">
+                <div class="lg:w-2/3 text-center mx-auto">
+                    <h1 class="text-gray-900 dark:text-white font-bold text-5xl md:text-6xl xl:text-7xl">Shaping a world
+                        with <span class="text-primary dark:text-white">reimagination.</span></h1>
+                    <p class="mt-8 text-gray-700 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Odio incidunt nam itaque sed eius modi error totam sit illum. Voluptas doloribus asperiores
+                        quaerat aperiam. Quidem harum omnis beatae ipsum soluta!</p>
+                    <div class="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
+                        <a href="#"
+                            class="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max">
+                            <span class="relative text-base font-semibold text-white">Get started</span>
+                        </a>
+                        <a href="#"
+                            class="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max">
+                            <span class="relative text-base font-semibold text-primary dark:text-white">Learn more</span>
+                        </a>
+                    </div>
+                    <div class="hidden py-8 mt-16 border-y border-gray-100 dark:border-gray-800 sm:flex justify-between">
+                        <div class="text-left">
+                            <h6 class="text-lg font-semibold text-gray-700 dark:text-white">The lowest price</h6>
+                            <p class="mt-2 text-gray-500">Some text here</p>
+                        </div>
+                        <div class="text-left">
+                            <h6 class="text-lg font-semibold text-gray-700 dark:text-white">The fastest on the market</h6>
+                            <p class="mt-2 text-gray-500">Some text here</p>
+                        </div>
+                        <div class="text-left">
+                            <h6 class="text-lg font-semibold text-gray-700 dark:text-white">The most loved</h6>
+                            <p class="mt-2 text-gray-500">Some text here</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-12 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6">
+                    <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+                        <img src="./images/clients/microsoft.svg" class="h-12 w-auto mx-auto" loading="lazy"
+                            alt="client logo" width="" height="" />
+                    </div>
+                    <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+                        <img src="./images/clients/airbnb.svg" class="h-12 w-auto mx-auto" loading="lazy" alt="client logo"
+                            width="" height="" />
+                    </div>
+                    <div class="p-4 flex grayscale transition duration-200 hover:grayscale-0">
+                        <img src="./images/clients/google.svg" class="h-9 w-auto m-auto" loading="lazy" alt="client logo"
+                            width="" height="" />
+                    </div>
+                    <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+                        <img src="./images/clients/ge.svg" class="h-12 w-auto mx-auto" loading="lazy" alt="client logo"
+                            width="" height="" />
+                    </div>
+                    <div class="p-4 flex grayscale transition duration-200 hover:grayscale-0">
+                        <img src="./images/clients/netflix.svg" class="h-8 w-auto m-auto" loading="lazy" alt="client logo"
+                            width="" height="" />
+                    </div>
+                    <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+                        <img src="./images/clients/google-cloud.svg" class="h-12 w-auto mx-auto" loading="lazy"
+                            alt="client logo" width="" height="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="solution">
+        <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-sky-500">
+                <path fill-rule="evenodd"
+                    d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"
+                    clip-rule="evenodd" />
+                <path fill-rule="evenodd"
+                    d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"
+                    clip-rule="evenodd" />
+            </svg>
+
+            <div
+                class="space-y-6 justify-between text-gray-600 md:flex flex-row-reverse md:gap-6 md:space-y-0 lg:gap-12 lg:items-center">
+                <div class="md:5/12 lg:w-1/2">
+                    <img src="./images/pie.svg" alt="image" loading="lazy" width="" height="" class="w-full" />
+                </div>
+                <div class="md:7/12 lg:w-1/2">
+                    <h2 class="text-3xl font-bold text-gray-900 md:text-4xl dark:text-white">
+                        Nuxt development is carried out by passionate developers
+                    </h2>
+                    <p class="my-8 text-gray-600 dark:text-gray-300">
+                        Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
+                        Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia. <br> <br> Vitae error,
+                        quaerat officia delectus voluptatibus explicabo quo pariatur impedit, at reprehenderit aliquam a
+                        ipsum quas voluptatem. Quo pariatur asperiores eum amet.
+                    </p>
+                    <div class="divide-y space-y-4 divide-gray-100 dark:divide-gray-800">
+                        <div class="mt-8 flex gap-4 md:items-center">
+                            <div class="w-12 h-12 flex gap-4 rounded-full bg-indigo-100 dark:bg-indigo-900/20">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                    class="w-6 h-6 m-auto text-indigo-500 dark:text-indigo-400">
+                                    <path fill-rule="evenodd"
+                                        d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97zM6.75 8.25a.75.75 0 01.75-.75h9a.75.75 0 010 1.5h-9a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H7.5z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="w-5/6">
+                                <h3 class="font-semibold text-lg text-gray-700 dark:text-indigo-300">Chat Anytime</h3>
+                                <p class="text-gray-500 dark:text-gray-400">Asperiores nemo possimus nesciunt quam mollitia.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="pt-4 flex gap-4 md:items-center">
+                            <div class="w-12 h-12 flex gap-4 rounded-full bg-teal-100 dark:bg-teal-900/20">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                    class="w-6 h-6 m-auto text-teal-600 dark:text-teal-400">
+                                    <path fill-rule="evenodd"
+                                        d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="w-5/6">
+                                <h3 class="font-semibold text-lg text-gray-700 dark:text-teal-300">Real Time Location</h3>
+                                <p class="text-gray-500 dark:text-gray-400">Asperiores nemo possimus nesciunt quam mollitia.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
